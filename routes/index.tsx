c499c7ca30913c089d36fd012de8e511dd05ba48
Dan Negara <dannegareh@gmail.com>
2020-05-27 02:06:37 +0300

    removing useless routes

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import EventsList from '../components/events-list';
-import DetailScreen from '../components/detail-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 import Login from '../components/login';
 import Register from '../components/register';
 import Scanner from '../components/scanner';
